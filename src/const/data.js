62b4eb351a814353d6cc3741647b5a9c038296cc
Kevin Gutiérrez <kevinGutierrez6595@gmail.com>
2021-12-02 23:15:31 -0600

    update img and url proyects

--- src/const/data.js
+++ src/const/data.js
@@ -1,49 +1,41 @@
 export const PORFOLIO_LIST = [
   {
-    href: "https://mini-viajes.web.app/",
+    href: 'https://mini-viajes.web.app/',
     text: {
-      one: "Pagina web para la agencia de viajes MiniViajesMX",
-      two: "Tecnologías:  ReactJS, Rudux y firebase",
+      one: 'Pagina web para la agencia de viajes MiniViajesMX',
+      two: 'Tecnologías:  ReactJS, Rudux y firebase',
     },
-    img: "https://tlaxdevs.com/_next/image?url=%2Fminiviajes.svg&w=256&q=75",
+    img: '/miniviajes.svg',
   },
   {
-    href: "https://play.google.com/store/apps/details?id=com.mx.ztiluz&hl=es_MX&gl=US",
+    href: 'https://play.google.com/store/apps/details?id=com.mx.ztiluz&hl=es_MX&gl=US',
     text: {
-      one: "Aplicaión android Ztiluz",
-      two: "Tecnologías:  koltin",
+      one: 'Aplicaión android Ztiluz',
+      two: 'Tecnologías:  koltin',
     },
-    img: "/AppZtiluz.svg",
+    img: '/AppZtiluz.svg',
   },
   {
-    href: "https://ztiluz.com",
+    href: 'https://ztiluz-web.web.app/',
     text: {
-      one: "Pagina web para la ztiluz",
-      two: "Tecnologías:  ReactJS, Redux, firebase y nodeJs",
+      one: 'Pagina web para la ztiluz',
+      two: 'Tecnologías:  ReactJS, Redux, firebase y nodeJs',
     },
-    img: "/ztiluzweb.svg",
+    img: '/ztiluzweb.svg',
   },
-  {
-    href: "https://tlaxdevs.com",
-    text: {
-      one: "Pagina web de Tlaxdevs",
-      two: "Tecnologías:  ReactJS",
-    },
-    img: "TlaxDev.svg",
-  },
-];
+]
 
 export const CURSES_LIST = [
   {
-    urlImg: "/curses/diploma-basico-javascript-1.jpg",
-    name: "Javascript basico",
+    urlImg: '/curses/diploma-basico-javascript-1.jpg',
+    name: 'Javascript basico',
   },
-  { urlImg: "/curses/diploma-frontend-developer-1.jpg" },
-  { urlImg: "/curses/diploma-frontend-developer-practico-1.jpg" },
-  { urlImg: "/curses/diploma-javascript-practico-1.jpg" },
-  { urlImg: "/curses/diploma-react-1.jpg" },
-  { urlImg: "/curses/diploma-react-avanzado-1.jpg" },
-  { urlImg: "/curses/diploma-react-native-1.jpg" },
-  { urlImg: "/curses/diploma-react-patrones-render-1.jpg" },
-  { urlImg: "/curses/diploma-react-practico-1.jpg" },
-];
+  { urlImg: '/curses/diploma-frontend-developer-1.jpg' },
+  { urlImg: '/curses/diploma-frontend-developer-practico-1.jpg' },
+  { urlImg: '/curses/diploma-javascript-practico-1.jpg' },
+  { urlImg: '/curses/diploma-react-1.jpg' },
+  { urlImg: '/curses/diploma-react-avanzado-1.jpg' },
+  { urlImg: '/curses/diploma-react-native-1.jpg' },
+  { urlImg: '/curses/diploma-react-patrones-render-1.jpg' },
+  { urlImg: '/curses/diploma-react-practico-1.jpg' },
+]
