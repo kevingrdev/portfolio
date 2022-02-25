8e06443a5987edc22ffba1c9a5fa2be799fddf5d
Kevin Gutiérrez <kevinGutierrez6595@gmail.com>
2022-02-25 15:33:54 -0600

    [update] SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
 import 'styles/App.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height"
+        />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Soy kevin Gutiérrez y esté es mi portafolio de trabajos destacados."
+        />
+        {/* <meta name="twitter:title" content="KEVIN - DEVELOPER" />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:locale" content="es" />
+        <meta property="og:url" content="https://kevingr.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="KEVIN - DEVELOPER" />
+        <meta property="og:title" content="KEVIN - DEVELOPER" /> */}
+
+        <meta property="og:image" content="web.png" />
+        <meta name="author" content="Kevin Guti├®rrez" />
+
+        <title>KEVIN - DEVELOPER</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { Head } from 'next/head'
 import { Presentation } from 'components/Presentation'
 import { TextSlide } from 'components/TextSlide'
 import { Portfolio } from 'components/Portfolio'
@@ -5,20 +6,22 @@ import { Curses } from 'components/Curses'
 import { Footer } from 'components/Footer'
 import { Nav } from 'components/Nav'
 
-export default function home() {
+export default function Home() {
   return (
-    <main className="layout">
-      <header className="header">
-        <Nav />
-        <TextSlide />
-        <h2>mi nombre es Kevin</h2>
-      </header>
-      <main>
-        <Presentation />
-        <Portfolio />
-        <Curses />
+    <div>
+      <main className="layout">
+        <header className="header">
+          <Nav />
+          <TextSlide />
+          <h2>mi nombre es Kevin</h2>
+        </header>
+        <main>
+          <Presentation />
+          <Portfolio />
+          <Curses />
+        </main>
+        <Footer />
       </main>
-      <Footer />
-    </main>
+    </div>
   )
 }
